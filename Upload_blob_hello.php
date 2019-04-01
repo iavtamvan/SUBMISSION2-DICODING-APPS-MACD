bc2e265a090861a004072ae44e387a6a089e8649
iavtamvan <ade.fajr.ariav@gmail.com>
2019-04-01 20:49:26 +0700

    adding detection

--- Upload_blob_hello.php
+++ Upload_blob_hello.php
@@ -154,6 +154,6 @@ else
 ?>
 
 
-<form method="post" action="index.php?Cleanup&containerName=<?php echo $containerName; ?>">
+<form method="post" action="Upload_blob_hello.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
 </form>
