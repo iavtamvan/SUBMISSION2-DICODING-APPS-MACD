795226d8298639a09a8d0d79636a2afbfb6ed9ac
iavtamvan <ade.fajr.ariav@gmail.com>
2019-04-01 17:19:47 +0700

    first

--- index.php
+++ index.php
@@ -155,6 +155,6 @@ else
 ?>
 
 
-<form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
+<form method="post" action="index.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
 </form>
